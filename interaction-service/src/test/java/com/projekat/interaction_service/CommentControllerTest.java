331444f846e4072994406658c6ecf5784d7c8736
lhadzic1 <lhadzic1@etf.unsa.ba>
2026-05-07 23:34:35 +0200

    Added integration tests for Comments

--- interaction-service/src/test/java/com/projekat/interaction_service/CommentControllerTest.java
+++ interaction-service/src/test/java/com/projekat/interaction_service/CommentControllerTest.java
@@ -2,7 +2,6 @@ package com.projekat.interaction_service;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
