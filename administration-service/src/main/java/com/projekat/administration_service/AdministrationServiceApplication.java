50bfe4037918e229746cf373154b89e61517aa9e
aroksa2 <aroksa2@etf.unsa.ba>
2026-05-14 17:12:34 +0200

    Add Eureka client configuration and load balancing support

--- administration-service/src/main/java/com/projekat/administration_service/AdministrationServiceApplication.java
+++ administration-service/src/main/java/com/projekat/administration_service/AdministrationServiceApplication.java
@@ -2,6 +2,9 @@ package com.projekat.administration_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
 
 @SpringBootApplication
 public class AdministrationServiceApplication {
