4e6ffc302f60b165a0ca2d6386724f97649b3aec
lhadzic1 <lhadzic1@etf.unsa.ba>
2026-06-04 13:55:43 +0200

    Fixed hardcoded frontend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // loadEnv ucitava odgovarajuci .env fajl na osnovu "mode" (development ili production)
+  // treci parametar '' znaci da trazi sve varijable a ne samo one sa VITE_ prefiksom u Node okruzenju
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE || 'http://localhost:8080',
+          changeOrigin: true,
+        }
       }
     }
   }
